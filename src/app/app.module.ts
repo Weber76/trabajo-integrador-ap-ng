dfa1f35c764ca1f8bdcb416e05ad35d6638ad429
Víctor Andrés Romano <victor.andres.romano@gmail.com>
2023-04-09 14:46:28 -0300

    Creados componentes header, logo-ap y social. Agregados Bootstrap, Fontawesome, JQuery y Popper. Contenidos y estilos agregados en los componentes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule
